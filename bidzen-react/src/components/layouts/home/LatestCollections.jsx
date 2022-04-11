7314cc18471908ea131864241cfcad64f0966b9a
Cristian <ch.lopez@uniandes.edu.co>
2022-04-11 11:17:41 -0500

    about game in home updated

--- bidzen-react/src/components/layouts/home/LatestCollections.jsx
+++ bidzen-react/src/components/layouts/home/LatestCollections.jsx
@@ -2,64 +2,63 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import imgdetail1 from './Images/cards.png'
+import avt1 from './Images/avt-4.jpg'
 
 const LatestCollections = props => {
     const data = props.data;
     return (
         <section className="tf-latest-collections tf-section bg-color-2">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="sc-heading style-2">
-                        <div className="content-left">
-                            <div className="inner">
-                                <h3>About Game</h3>
-                                <p className="desc">Most popular cards game in market </p>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="sc-heading style-2">
+                            <div className="content-left">
+                                <div className="inner">
+                                    <h3>About Game</h3>
+                                    <p className="desc">Most popular cards game in market </p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-md-12">
-                    <div className="swiper-container latest-coll style-2">
-                        <div className="swiper-wrapper">
-                        {/* <Swiper
-                            modules={[Navigation, Pagination, Scrollbar, A11y]}
-                            spaceBetween={30}
 
-                            breakpoints={{
-                                0: {
-                                    slidesPerView: "auto",
-                                    },
-                                767: {
-                                    slidesPerView: "auto",
-                                },
-                                991: {
-                                    slidesPerView: "auto",
-                                },
-                                1300: {
-                                    slidesPerView: "auto",
-                                    },
-                                }}
-                            navigation
-                            pagination={{ clickable: true }}
-                            scrollbar={{ draggable: true }}
-                        >
-                        {
-                            data.map((item,index) => (
-                                <SwiperSlide className={item.styleclass} key={index}>
-                                    <CollectionsItem item={item} />
-                                </SwiperSlide>
-                            ))
-                        }
-                    </Swiper> */}
+                    <section className="tf-section item-details-page">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-xl-6 col-lg-12 col-md-12">
+                                    <div className="item-media">
+                                        <div className="media">
+                                            <img src={imgdetail1} alt="" />
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                                <div className="col-xl-6 col-lg-12 col-md-12">
+                                    <div className="content-item">
+                                        <h3 >Mythic cards game</h3>
+                                        <p className="mg-bt-30">We see a better way to value the game cards. Besides, you can do transactions during the game eith your cards according to the statistics and the way you have played your cards. The more cards you acquire, the more possibilities to trade your cards using criptos. Also, your cards get more valued when you level across the game!</p>
 
-                    
+
+                                        <h2 >PLAY</h2>
+                                        <p className="mg-bt-30">Use your cards to play battles in the game. Beat thousands of oponents around the world and level up to gain respect, card value and unlock better cards and upgrades. <br></br> Play Mythic Cards from any device at any spot globally. You just have to get an internet connection and log in!</p>
+
+                                        <h2 >EXCHANGE / SELL</h2>
+                                        <p className="mg-bt-30">Buy your cards, level them up to increase its value and its ranking. Once you think that your card is ready to be sold, you can sell it or exchange it with any other one. <br></br> Also, you can keep your cards to level up your ranking with other players. Your personal score will also make your cards to be more valued. Be strategic about how to manage your cards, to defeat other players and gain money.</p>
+
+                                        <h2 >WIN</h2>
+                                        <p className="mg-bt-30">Learn how to recognize the different types of cards. Being strategic is the key. Use cards characteristics such as rarity, experience, level and aabilities to improve the statistics of the character's attributes.<br></br>You can use affect statistics of every type of card like speed, armor, magic attacks, ranged attacks and so on. Statistics will be varying according to the use of your card's abilities: rust, poison, flying, dispel and many more!</p>
+
+
+
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+                    </section>   
+
+                </div >
+            </div >
+        </section >
     );
 };
 
@@ -99,7 +98,7 @@ const CollectionsItem = props => (
                     </div>
                     <div className="button-wishlish">
                         <Link to="#" className=" wishlish"><i
-                                className="fas fa-heart"></i></Link>
+                            className="fas fa-heart"></i></Link>
                         <span>{props.item.wishlist}</span>
                     </div>
                 </div>
